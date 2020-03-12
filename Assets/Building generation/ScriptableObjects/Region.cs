d129b79a0c3d0022e5ca7133775eb237c972b55f
msaouter <msaouter@gmail.com>
2020-03-12 16:41:55 +0100

    [ADD] Building prefab + Scripts + ScriptablesObjects

---
+++ Assets/Building generation/ScriptableObjects/Region.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Region", menuName = "ScriptableObjects/RegionManager", order = 1)]
+
+public class Region : ScriptableObject
+{
+    public int minBuildSpace;
+    public int maxBuildSpace;
+    public float minRadius;
+    public float maxRadius;
+}
