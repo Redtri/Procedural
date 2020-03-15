72a371f18583248b9249bc9d748fd1da69ade59c
unknown <Administrateur@CNA-AN-FD045.cna.local>
2020-03-15 15:52:28 +0100

    [FIX] New working road generator

--- Assets/Scripts/RoadGenerator.cs
+++ Assets/Scripts/RoadGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 [ExecuteInEditMode]
 public class RoadGenerator : MonoBehaviour
@@ -10,6 +11,7 @@ public class RoadGenerator : MonoBehaviour
     public int nbPeriphRoads;
     public Vector2 distRange;
     public Vector2 nbCornersRange;
+    public Vector2 dirNoise;
 
     public GameObject cornerPrefab;
 
@@ -21,29 +23,33 @@ public class RoadGenerator : MonoBehaviour
     {
         public int nbCorners;
         public Transform container;
+        public Vector3 origin;
         public LineRenderer lineRenderer;
-        public List<GameObject> corners = new List<GameObject>();
-        public Road(string name, Transform trsf, LineRenderer lineRender, Transform topParent = null)
+        public LinkedList<GameObject> corners = new LinkedList<GameObject>();
+        public Road(string name, Transform trsf, LineRenderer lineRender, Vector3 startPoint, Transform topParent = null)
         {
             container = trsf;
             container.gameObject.name = name;
+            origin = startPoint;
             lineRenderer = lineRender;
+            lineRenderer.positionCount = 1;
             container.parent = topParent;
-            corners = new List<GameObject>();
+            corners = new LinkedList<GameObject>();
         }
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             GenerateRoads();
+        }
+
+        if (Input.GetKeyDown((KeyCode.C)))
+        {
+            ClearRoads(roads);
+            ClearRoads(periphRoads);
         }
     }
 
@@ -53,63 +59,115 @@ public class RoadGenerator : MonoBehaviour
         periphRoads = new List<Road>(nbPeriphRoads);
         for(int i = 0; i < nbPeriphRoads; ++i) {
             GameObject newRoadGO = new GameObject();
-            Road currentRoad = new Road("PeriphRoad_" + i, newRoadGO.transform, newRoadGO.AddComponent<LineRenderer>(), this.transform);
+            Road currentRoad = new Road("PeriphRoad_" + i, newRoadGO.transform, newRoadGO.AddComponent<LineRenderer>(), Vector3.zero, this.transform);
             periphRoads.Add(currentRoad);
         }
 
         for (int i = 0; i < nbStraightRoads; ++i) {
-            //Calculating angle in which the road points to
+            //Calculating angle in which the road points to and the direction associated
             float currentAngle = (360f / nbStraightRoads) * i;
-            Vector2 direction = new Vector2(Mathf.Cos(Mathf.Deg2Rad * currentAngle), Mathf.Sin(Mathf.Deg2Rad * currentAngle));
+            Vector3 direction = new Vector3(Mathf.Cos(Mathf.Deg2Rad * (currentAngle + Random.Range(dirNoise.x, dirNoise.y))),
+                                                        0f, 
+                                                        Mathf.Sin(Mathf.Deg2Rad * (currentAngle + Random.Range(dirNoise.x, dirNoise.y)))
+                                                        );
 
-            //Spawning a Road GameObject that will contain nbCorners corners
-            GameObject newRoadGO = new GameObject();
-            Road currentRoad = new Road("StraightRoad_" + i, newRoadGO.transform, newRoadGO.AddComponent<LineRenderer>(),this.transform);
+            //Creating new road
+            Road currentRoad = CreateRoad(center.position, direction);
 
-            //Setting a random amount of corners
-            currentRoad.nbCorners = Random.Range((int)nbCornersRange.x, (int)nbCornersRange.y);
-
-            GenerateCorners(i, currentRoad, new Vector3(direction.x, 0f, direction.y));
-
+            //Generating corners inside the road
+            GenerateCorners(i, currentRoad, direction);
 
             roads.Add(currentRoad);
         }
     }
 
+    public void ClearRoads(List<Road> tRoads)
+    {
+        foreach(Road road in tRoads)
+        {
+            ClearRoad(road);
+            Destroy(road.container.gameObject);
+        }
+
+        tRoads.Clear();
+    }
+
+    private void ClearRoad(Road road)
+    {
+        foreach (GameObject corner in road.corners)
+        {
+            if(corner)
+                Destroy(corner);
+        }
+        road.corners.Clear();
+        road.lineRenderer.positionCount = 1;
+    }
+
+    private Road CreateRoad(Vector3 origin, Vector3 direction)
+    {
+        //Spawning a Road GameObject that will contain nbCorners corners
+        GameObject newRoadGo = new GameObject();
+        Road newRoad = new Road("StraightRoad_" + roads.Count, newRoadGo.transform, newRoadGo.AddComponent<LineRenderer>(), origin, this.transform);
+
+        //Setting a random amount of corners
+        newRoad.nbCorners = Random.Range((int)nbCornersRange.x, (int)nbCornersRange.y);
+        
+        return newRoad;
+    }
+
     private void GenerateCorners(int roadIndex, Road currentRoad, Vector3 direction)
     {
-        Debug.Log(direction);
         //At this point, the previous corner is the origin of the map generator
-        Vector3 previousCorner = center.position;
-        //Also, creating an array for the line renderer points as large as the number of corners
-        Vector3[] lineRenderAnchors = new Vector3[currentRoad.nbCorners + 1];
-        currentRoad.lineRenderer.positionCount = currentRoad.nbCorners + 1;
+        Vector3 previousLocation = currentRoad.origin;
 
         //Iterating to create as many corners as defined by nbCorners
-        for (int currentCorner = 0; currentCorner < currentRoad.nbCorners; ++currentCorner) {
-            lineRenderAnchors[currentCorner] = previousCorner;
-            Vector2 tmpDir = direction;// new Vector2(Mathf.Cos(Random.Range(0, 5f)), Mathf.Sin(Random.Range(0, 5f)));
-            GameObject newCornerGO = Instantiate(cornerPrefab, currentRoad.container.transform);
+        for (int cornerIndex = 0; cornerIndex < currentRoad.nbCorners; ++cornerIndex)
+        {
 
-            //Offseting the corner from the last one within range and adding it to the list
-            newCornerGO.transform.position = previousCorner + new Vector3(tmpDir.x, 0f, tmpDir.y) * Random.Range((int)distRange.x, (int)distRange.y);
-            currentRoad.corners.Add(newCornerGO);
-
-            previousCorner = currentRoad.corners[currentCorner].transform.position;
-
-            //The current corner represents an index for each peripheral road
-            /*if (currentCorner < nbPeriphRoads && roadIndex > 0) {
-                periphRoads[currentCorner].lineRenderer.positionCount += 1;
-                periphRoads[currentCorner].lineRenderer.SetPosition(periphRoads[currentCorner].lineRenderer.positionCount - 1, roads[roadIndex - 1].corners[currentCorner].transform.position);
-                /*if (roadIndex == nbStraightRoads - 1) {
-                    //Linking the corners of the last straight road to the first road ones
-                    periphRoads[currentCorner].lineRenderer.SetPosition(1, roads[roadIndex].corners[currentCorner].transform.position);
-                }
-            }*/
+            CreateCorner(currentRoad, previousLocation, direction, cornerIndex, roadIndex);
+            previousLocation = currentRoad.corners.Last.Value.transform.position;
         }
-        lineRenderAnchors[lineRenderAnchors.Length - 1] = previousCorner;
+    }
 
-        currentRoad.lineRenderer.SetPositions(lineRenderAnchors);
+    private void CreateCorner(Road currentRoad, Vector3 previousLocation, Vector3 direction, int cornerIndex, int roadIndex)
+    {
+        //STRAIGHT ROAD
+        GameObject newCornerGo = Instantiate(cornerPrefab, previousLocation + direction * Random.Range((int)distRange.x, (int)distRange.y), Quaternion.identity, currentRoad.container.transform);
+        LineRenderer mainRoadlineRenderer = currentRoad.lineRenderer;
+
+        currentRoad.corners.AddLast(newCornerGo);
+        
+        //LINE RENDERER
+        mainRoadlineRenderer.positionCount += 1;
+        mainRoadlineRenderer.SetPosition(cornerIndex+1, newCornerGo.transform.position);
+
+        //PERIPHERAL ROADS
+        //The current corner represents an index for each peripheral road
+        if (cornerIndex < nbPeriphRoads)
+        {
+            LineRenderer secRoadLineRenderer = periphRoads[cornerIndex].lineRenderer;
+            if (roadIndex > 0)
+            {
+                secRoadLineRenderer.positionCount += 1;
+                secRoadLineRenderer.SetPosition(roadIndex-1, periphRoads[cornerIndex].corners.Last.Value.transform.position);
+                if (roadIndex == nbStraightRoads - 1) {
+                    secRoadLineRenderer.SetPosition(roadIndex, newCornerGo.transform.position);
+                    secRoadLineRenderer.positionCount += 1;
+                    secRoadLineRenderer.SetPosition(secRoadLineRenderer.positionCount-1, periphRoads[cornerIndex].corners.First.Value.transform.position);
+                }
+            }
+            periphRoads[cornerIndex].corners.AddLast(newCornerGo);
+        }
+    }
+
+    private void BindCorners(LineRenderer render, GameObject cornerA, GameObject cornerB)
+    {
+        
+    }
+    
+    private void BindCorner()
+    {
+        
     }
 
     private void OnDrawGizmos()
